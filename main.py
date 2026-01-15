79da3799450ed8d9d8daf3d48235b79f01e7702b
Szanyi Márk <szanyi.mark@sonrisa.hu>
2026-01-15 16:37:57 +0100

    add Docker

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 @app.get("/")
+def read_root():
+    return RedirectResponse(url="/docs")
+
+@app.get("/hello")
 def read_root():
     return {"Hello": "World"}
 
